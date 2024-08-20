7c59e6f72c0d7bac3f8f8383e4d713d98aba09de
smi <stefano.michelini@cetic.be>
2024-08-20 10:48:52 +0200

    Add CSP instance scaladoc

--- src/main/scala/csp/CarSeqProblem.scala
+++ src/main/scala/csp/CarSeqProblem.scala
@@ -13,6 +13,25 @@
 
 package csp
 
+/** This class represents an instance of the car sequencing problem (CSP).
+  *
+  * The CSP is concerned with finding a feasible production ordering of the given set of cars. Each
+  * car belongs to a configuration, and each configuration is defined by a subset of the available
+  * options (e.g. as air conditioning). For each option, two positive integers are provided: a
+  * subsequence length and the maximum number of cars using that option that can be produced in any
+  * subsequence of that length. An ordering is feasible if this subsequence constraint is satisfied
+  * for all options.
+  *
+  * @param nCars
+  *   total number of cars
+  * @param nOptions
+  *   number of different options each car can have
+  * @param nConf
+  *   total number of configurations, each defined by a subset of options
+  * @param maxCarsWithOptInSeq maximum number of cars in a subsequence with the given option
+  * @param optSeqLen subsequence length for the option constraint
+  * @param configs list of configurations
+  */
 case class CarSeqProblem(
   nCars: Int,
   nOptions: Int,
@@ -21,6 +40,8 @@ case class CarSeqProblem(
   optSeqLen: Array[Int],           // subsequence length
   configs: List[CarSeqConf]
 ) {
+
+  /** Checks whether this CSP instance is valid or not. */
   def isValid: Boolean = {
     // need one of these values for each option
     if ((maxCarsWithOptInSeq.length != nOptions) || (optSeqLen.length != nOptions)) return false
@@ -42,4 +63,11 @@ case class CarSeqProblem(
   }
 }
 
+/**
+ * Class defining a configuration for a CSP instance.
+ *
+ * @param id identification number
+ * @param nCarsWithConf total number of cars with this configuration
+ * @param optInConf 0-1 array defining which options belong to this configuration
+ */
 case class CarSeqConf(id: Int, nCarsWithConf: Int, optInConf: Array[Int])
