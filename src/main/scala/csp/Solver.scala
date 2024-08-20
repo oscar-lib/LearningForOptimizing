f3ef8bc30b20921a2a28baf474b102ebb7281292
smi <stefano.michelini@cetic.be>
2024-08-20 16:09:38 +0200

    Add core of solver logic

--- src/main/scala/csp/Solver.scala
+++ src/main/scala/csp/Solver.scala
@@ -13,8 +13,74 @@
 
 package csp
 
+import combinator._
+import oscar.cbls._
+import oscar.cbls.core.search.Neighborhood
+
+import scala.concurrent.duration.Duration
+
 case class Solver(oscarModel: Model, bandit: String) {
 
-  def solve(verbosity: Int, display: Boolean, getName: String, timeout: Int): Unit = ???
+  private val obj: Objective = oscarModel.obj
+
+  def solve(verbosity: Int, display: Boolean, getName: String, timeout: Int): Unit = {
+    val withTimeout = timeout < Int.MaxValue
+
+    val neighList: List[Neighborhood] = Nil
+
+    val banditNeighborhood: Neighborhood = bandit.toLowerCase() match {
+      case "bandit" => BanditCombinator(neighList, ???, 0, obj, ???) saveBestAndRestoreOnExhaust obj
+
+      case "banditaftermove" =>
+        BanditCombinator(neighList, ???, 0, obj, ???) saveBestAndRestoreOnExhaust obj
+
+      case "banditrollingaverage" =>
+        BanditCombinator(neighList, ???, 0, obj, ???) saveBestAndRestoreOnExhaust obj
+
+      case "epsilongreedy" => new EpsilonGreedyBandit(neighList)
+
+      case "epsilongreedynew" => new EpsilonGreedyBanditNew(neighList)
+
+      case "ucbnew" => new UCB1(neighList)
+
+      case "bestslopefirst" => bestSlopeFirst(neighList)
+
+      case "bestsslopefirstnew" => new BestSlopeFirstNew(neighList)
+
+      case "random" => new RandomCombinator(neighList)
+
+      case _ =>
+        println("warning: invalid bandit specified. Defaulting to bestSlopeFirst")
+        bestSlopeFirst(neighList)
+    }
+
+    var search = {
+      banditNeighborhood match {
+        case BanditCombinator(_, _, _, _, _, _, _, _) => banditNeighborhood
+        case _ =>
+          banditNeighborhood onExhaustRestartAfter (???, 0, obj, minRestarts =
+            if (withTimeout) Int.MaxValue else 15)
+      }
+    }
+
+    val c = oscarModel.constraintSystem
+    if (withTimeout) {
+      search = search.weakTimeout(Duration(timeout, "second")) saveBestAndRestoreOnExhaust obj
+    }
+    search.verbose = verbosity
+    search.doAllMoves(_ => c.isTrue, obj = obj)
+    if (verbosity > 1) {
+      search.profilingOnConsole()
+      println(obj)
+    }
+    println(oscarModel)
+
+    println("car sequence:" + oscarModel.carSequence.map(_.value).mkString(","))
+    println("bestObj=" + oscarModel.obj.value)
+    println(
+      if (c.violation.value == 0) "Problem solved"
+      else s"PROBLEM COULD NOT BE SOLVED: ${c.violation}"
+    )
+  }
 
 }
