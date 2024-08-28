6ad03baf6c310d776d8c6afaefbed55e0a3afe94
Augustin Delecluse <augustin.delecluse@hotmail.com>
2024-08-28 12:30:09 +0100

    fixed NeighborhoodStats constructor

--- src/main/scala/combinator/NeighborhoodStats.scala
+++ src/main/scala/combinator/NeighborhoodStats.scala
@@ -17,12 +17,11 @@ import oscar.cbls.core.search.{MoveFound, Neighborhood, NoMoveFound, SearchResul
 
 /** Lightweight store of information regarding the execution of a move
   *
-  * @param hasImproved whether the move has improved the solution quality or not
   * @param foundMove whether a move has been found or not
   * @param slope the slope, i.e., the ratio between the quality of improvement and its performance cost
   * @param timeNano the elapsed time
   */
-case class NeighborhoodStats(hasImproved: Boolean, foundMove: Boolean, slope: Double, timeNano: Long)
+case class NeighborhoodStats(foundMove: Boolean, slope: Double, timeNano: Long)
 
 object NeighborhoodStats {
 
@@ -40,7 +39,6 @@ object NeighborhoodStats {
         case NoMoveFound  => false
         case MoveFound(_) => true
       },
-      NeighborhoodUtils.lastCallGain(neighborhood) > 0,
       NeighborhoodUtils.slope(neighborhood),
       NeighborhoodUtils.lastCallDuration(neighborhood)
     )
