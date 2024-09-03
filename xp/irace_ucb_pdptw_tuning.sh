04ab136be507492e5b44de7390f290f42247e307
smi <stefano.michelini@cetic.be>
2024-09-03 16:57:17 +0100

    Update sample script

--- xp/irace_ucb_pdptw_tuning.sh
+++ xp/irace_ucb_pdptw_tuning.sh
@@ -1,2 +1,7 @@
 #run from top level directory
+DIRECTORY=pdptw-arena
+if [ ! -d "$DIRECTORY" ]; then
+  mkdir $DIRECTORY
+fi
+sbt clean && sbt assembly
 irace --parallel 2 --train-instances-file examples/pdptw/training100.txt
