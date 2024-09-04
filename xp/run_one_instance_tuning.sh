44f8835306f3ab3c164cf30c88bb02e808edcd0b
smi <stefano.michelini@cetic.be>
2024-09-04 11:16:41 +0100

    improve root variable definition

--- xp/run_one_instance_tuning.sh
+++ xp/run_one_instance_tuning.sh
@@ -2,7 +2,7 @@
 # usage: ./script [instance] [bandit] [timeout]
 # example: ./xp/run_one_instance.sh bench/LC1_2_1.txt epsilongreedy 1
 params=$*
-root=".."
+root="$(dirname "$(dirname "$(realpath "$0")")")"
 launch_solver="java -jar $root/target/scala-2.13/learningforoptimizing-assembly-0.1.0-SNAPSHOT.jar solveInstance"
 output=`$launch_solver ${params}`
 # post process to extract only the relevant information
