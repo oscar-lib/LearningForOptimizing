f19035b3bea14310556c796de73701b30d903cf2
smi <stefano.michelini@cetic.be>
2024-09-05 10:43:58 +0100

    Edit irace script to copy .csv file to correct location

--- xp/irace_tuning.sh
+++ xp/irace_tuning.sh
@@ -20,5 +20,15 @@ if [ ! -d "$REPO_ROOT/$DATA_DIRECTORY" ]; then
   mkdir "$REPO_ROOT/$DATA_DIRECTORY"
 fi
 
+#copy .csv file if not there
+CSV="$REPO_ROOT"/"$DATA_DIRECTORY"/bks/"$PROBLEM"_bks.csv
+if [ ! -f "$CSV" ]; then
+#  echo 1>&2 "$0: $CSV not found, copying from $REPO_ROOT/bks/"$PROBLEM"_bks.csv"
+  if [ ! -d "$REPO_ROOT/$DATA_DIRECTORY"/bks ]; then
+    mkdir "$REPO_ROOT/$DATA_DIRECTORY"/bks
+  fi
+  cp "$REPO_ROOT"/bks/"$PROBLEM"_bks.csv "$CSV"
+fi
+
 #sbt clean && sbt assembly
 irace -s "$SCENARIO" --target-runner "$TARGET_RUNNER" --parallel 4 --train-instances-file "$DATA_DIR"/$PROBLEM/training$TRAINING_SIZE.txt
