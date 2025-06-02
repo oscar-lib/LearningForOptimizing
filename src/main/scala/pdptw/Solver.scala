60619abf5a551a9a72518fe9fbdebd2f41f31c5b
Augustin Delecluse <augustin.delecluse@hotmail.com>
2025-06-02 10:38:41 +0200

    absolute path for pdptw bks

--- src/main/scala/pdptw/Solver.scala
+++ src/main/scala/pdptw/Solver.scala
@@ -295,12 +295,14 @@ case class Solver(oscarModel: Model, in: SolverInput) {
     println("bestObj=" + oscarModel.objectiveFunction.value)
     //println(recorder)
     val instanceName = Paths.get(fileName).getFileName.toString
-    val bestKnownSolution =
-      recorder.getBestKnownSolution("bks/pdptw_bks.csv", instanceName).getOrElse(0.0)
-    //val gapOverTime = recorder.primalGapOverTime(bestKnownSolution, timeout)
-    //println(f"primalGapOverTime=" + gapOverTime.map(e => f"(t=${e._1}%.3f-v=${e._2}%.6f)").mkString("[", "-", "]"))
     val realSolutionOverTime = recorder.realObjectiveTimeStamp
     println(f"solOverTime=" + realSolutionOverTime.map(e => f"(t:${e._1}%.3f-v:${e._2}%.3f)").mkString("[", "-", "]"))
+    val currentDirectory = System.getProperty("user.dir")
+    val rootDir = currentDirectory.split("LearningForOptimizing")(0)
+    val bestKnownSolution =
+      recorder.getBestKnownSolution(rootDir + "/LearningForOptimizing/bks/pdptw_bks.csv", instanceName).getOrElse(0.0)
+    //val gapOverTime = recorder.primalGapOverTime(bestKnownSolution, timeout)
+    //println(f"primalGapOverTime=" + gapOverTime.map(e => f"(t=${e._1}%.3f-v=${e._2}%.6f)").mkString("[", "-", "]"))
     val integralPrimalGap = recorder.integralPrimalGap(bestKnownSolution, timeout)
     println(f"integralPrimalGap=$integralPrimalGap%.3f".replace(',','.'))
   }
