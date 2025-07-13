6ce56aa3ea135e2bf851842c3c752db9af404817
Augustin Delecluse <augustin.delecluse@hotmail.com>
2025-07-13 14:14:57 +0200

    10 runs for each bandit

--- src/main/scala/csp/Solver.scala
+++ src/main/scala/csp/Solver.scala
@@ -104,6 +104,9 @@ case class Solver(cspModel: Model, in: SolverInput) {
 
       case "random" => new RandomCombinator(neighList)
 
+      case "roundrobin" =>
+        roundRobin(neighList.zip((0 to neighList.length).map(i => 1)))
+
       case _ =>
         println("warning: invalid bandit specified. Defaulting to bestSlopeFirst")
         bestSlopeFirst(neighList)
